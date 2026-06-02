b17dd8fe00c57b7595c5fc9e255afe19970e20f2
hemu-03 <hemuofficial03@gmail.com>
2026-06-02 16:15:55 +0530

    day 7

---
+++ Car.java
@@ -0,0 +1,20 @@
+class Car{
+      String name;
+        int speed;
+    void brand(){
+        System.out.println(name+ "" + ""+ speed);
+
+    }
+    public static void main(String[] args) {
+        Car c1= new Car();
+        c1.name="bmw";
+        c1.speed=224;
+
+        c1.brand();
+    
+
+
+
+
+    }
+}
